cd461ab04740423288affdee85ff2ce982a0cb01
startnothings <727792410@qq.com>
2019-03-07 23:18:13 +0800

    Initial commit

---
+++ quit.py
@@ -0,0 +1,15 @@
+import sys
+import threading
+import time
+
+
+def a():
+    while 1:
+        time.sleep(1)
+
+t = threading.Thread(target=a)
+t.setDaemon(1)
+t.start()
+t.join()
+
+sys.exit('goodbye')
